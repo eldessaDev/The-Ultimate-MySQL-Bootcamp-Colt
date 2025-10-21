56d6fc690be5e7fed631aed6d277919dc4829a20
eldessaDev <137922024+eldessaDev@users.noreply.github.com>
2025-10-21 14:13:03 -0400

    Constraint Done!

--- 09_Constraint.sql
+++ 09_Constraint.sql
@@ -13,3 +13,58 @@
     CREATE TABLE palindromes (
      word VARCHAR(100) CHECK(REVERSE(word) = word)
     )
+
+-- NAMED CONSTRAINT
+    CREATE TABLE users2 (
+    username VARCHAR(20) NOT NULL,
+    age INT,
+    CONSTRAINT age_not_negative CHECK (age >= 0)
+    );
+    
+    CREATE TABLE palindromes2 (
+    word VARCHAR(100),
+    CONSTRAINT word_is_palindrome CHECK(REVERSE(word) = word)
+    );
+
+-- MULTIPLE COLUMS CONSTRAINT
+    CREATE TABLE companies (
+    name VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    CONSTRAINT name_address UNIQUE (name , address)
+    );
+    
+    CREATE TABLE houses (
+    purchase_price INT NOT NULL,
+    sale_price INT NOT NULL,
+    CONSTRAINT sprice_gt_pprice CHECK(sale_price >= purchase_price)
+    );
+
+-- ALTER TABLE
+    ALTER TABLE companies 
+    ADD COLUMN phone VARCHAR(15);
+    
+    ALTER TABLE companies
+    ADD COLUMN employee_count INT NOT NULL DEFAULT 1;
+
+    -- DROP
+        ALTER TABLE companies DROP COLUMN phone;
+    
+    -- RENAMING
+        RENAME TABLE companies to suppliers;
+
+
+        ALTER TABLE suppliers RENAME TO companies;
+
+
+        ALTER TABLE companies
+        RENAME COLUMN name TO company_name;
+
+    -- MODIFY
+        ALTER TABLE companies
+        MODIFY company_name VARCHAR(100) DEFAULT 'unknown';
+    
+    -- ALTER TABLE CONSTRAINT
+        ALTER TABLE houses 
+        ADD CONSTRAINT positive_pprice CHECK (purchase_price >= 0);
+
+        ALTER TABLE houses DROP CONSTRAINT positive_pprice;
