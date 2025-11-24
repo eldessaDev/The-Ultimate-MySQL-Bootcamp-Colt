fa75e078edffef2b59a84ea613fabcdc3ce131b5
eldessaDev <137922024+eldessaDev@users.noreply.github.com>
2025-11-24 18:31:11 -0500

    done with one to many

--- 10_OneToManyJoins.sql
+++ 10_OneToManyJoins.sql
@@ -15,3 +15,17 @@
 -- LEFT JOIN
     SELECT first_name, last_name, order_date, amount FROM customers
     LEFT JOIN orders ON orders.customer_id = customers.id;
+-- LEFT JOIN WITH GROUP BY
+    SELECT first_name, last_name, IFNULL(SUM(amount), 0) AS money_spent
+    FROM customers
+    LEFT JOIN orders ON customers.id = orders.customer_id
+    GROUP BY first_name , last_name;
+
+-- RIGHT JOIN
+    SELECT 
+    first_name, last_name, order_date, amount FROM customers
+    RIGHT JOIN orders ON customers.id = orders.customer_id;
+
+-- ON DELETE CASCADE
+     FOREIGN KEY(customer_id) REFERENCES customers(id) ON DELETE CASCADE
+
